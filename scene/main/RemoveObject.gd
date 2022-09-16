a948fbfdb696004d41b919b6d63d868dfdc3dd6a
bozar42 <5583771+Bozar@users.noreply.github.com>
2022-09-16 19:38:19 +0800

    RemoveObject.remove() accepts a sprite as the parameter.

--- scene/main/RemoveObject.gd
+++ scene/main/RemoveObject.gd
@@ -5,28 +5,41 @@ class_name RemoveObject
 signal sprite_removed(sprite_data)
 
 
-func remove(main_tag: String, coord: IntCoord) -> void:
-	var remove_this: Sprite = $FindObject.get_sprite(main_tag, coord)
+func remove(remove_this: Sprite) -> void:
+	var store_state: StoreStateTemplate
+
 	if remove_this == null:
 		return
+	store_state = ObjectState.get_state(remove_this)
+	_free_sprite(remove_this, store_state.main_tag, store_state.coord)
 
-	emit_signal(SignalTag.SPRITE_REMOVED, BasicSpriteData.new(remove_this,
-			main_tag, SubTag.REMOVE_SPRITE, coord))
-	ObjectState.remove_state(remove_this)
-	remove_this.queue_free()
+
+func remove_by_coord(main_tag: String, coord: IntCoord) -> void:
+	var remove_this: Sprite = $FindObject.get_sprite(main_tag, coord)
+
+	if remove_this == null:
+		return
+	_free_sprite(remove_this, main_tag, coord)
 
 
 func remove_actor(coord: IntCoord) -> void:
-	remove(MainTag.ACTOR, coord)
+	remove_by_coord(MainTag.ACTOR, coord)
 
 
 func remove_building(coord: IntCoord) -> void:
-	remove(MainTag.BUILDING, coord)
+	remove_by_coord(MainTag.BUILDING, coord)
 
 
 func remove_trap(coord: IntCoord) -> void:
-	remove(MainTag.TRAP, coord)
+	remove_by_coord(MainTag.TRAP, coord)
 
 
 func remove_ground(coord: IntCoord) -> void:
-	remove(MainTag.GROUND, coord)
+	remove_by_coord(MainTag.GROUND, coord)
+
+
+func _free_sprite(sprite: Sprite, main_tag: String, coord: IntCoord) -> void:
+	emit_signal(SignalTag.SPRITE_REMOVED, BasicSpriteData.new(sprite, main_tag,
+			SubTag.REMOVE_SPRITE, coord))
+	ObjectState.remove_state(sprite)
+	sprite.queue_free()
