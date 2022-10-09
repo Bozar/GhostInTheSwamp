3899634188b1fc58b1862a922b03572e67d2bc79
bozar42 <5583771+Bozar@users.noreply.github.com>
2022-10-09 19:04:23 +0800

    Flank an NPC with ghost does not reduce MP cost.

--- library/LandPowerHelper.gd
+++ library/LandPowerHelper.gd
@@ -26,6 +26,8 @@ static func _get_spook_cost(pc_state: PcState, actor: Sprite) -> int:
 		cost += PcData.COST_SPOOK_EXTRA
 	if pc_state.count_item >= PcData.MAX_ITEM:
 		cost += PcData.COST_SPOOK_EXTRA
+	if pc_state.has_ghost:
+		return cost
 
 	var pc_coord := pc_state.coord
 	var actor_coord := actor_state.coord
