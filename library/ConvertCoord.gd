986c0195982c9b24a831223a1744449d917e4018
bozar42 <5583771+Bozar@users.noreply.github.com>
2023-07-21 10:37:22 +0800

    Verify hash coord inputs.

--- library/ConvertCoord.gd
+++ library/ConvertCoord.gd
@@ -9,6 +9,10 @@ const STEP_Y: int = 34
 const MOUSE_START_X := START_X - 10
 const MOUSE_START_Y := START_Y - 10
 
+const MIN_HASH_INPUT: int = 0
+const MAX_HASH_INPUT: int = 99
+const INVALID_HASH_COORD: int = -1
+
 const MAIN_TAG_TO_INT := {
 	MainTag.GROUND: 1,
 	MainTag.TRAP: 2,
@@ -45,11 +49,20 @@ static func coord_to_vector(coord: IntCoord, x_offset: int = 0,
 
 # Add Z index if necessary.
 static func hash_coord(coord: IntCoord, main_tag := "", z_index: int = 0) -> int:
+	if not _is_valid_hash_input([coord.x, coord.y, z_index]):
+		return INVALID_HASH_COORD
+
 	# 543210
-	# ZMXXYY | Z index, Main_tag, Coord.X, Coord.Y
-	var y := coord.y
-	var x := coord.x * pow(10, 2)
+	# ZMYYXX | z_index, main_tag, coord.y, coord.x
+	var x := coord.x
+	var y := coord.y * pow(10, 2)
 	var hash_tag: int = MAIN_TAG_TO_INT.get(main_tag, 0) * pow(10, 4)
 	var hash_layer := z_index * pow(10, 5)
+	return int(x + y + hash_tag + hash_layer)
 
-	return int(y + x + hash_tag + hash_layer)
+
+static func _is_valid_hash_input(inputs: Array) -> bool:
+	for i in inputs:
+		if (i < MIN_HASH_INPUT) or (i > MAX_HASH_INPUT):
+			return false
+	return true
